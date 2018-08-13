321961d3464fc4b298d65449c606e2b0c3f17e14
Dmitry <boldin.dmitry@gmail.com>
2018-08-13 11:32:49 +0200

    Добавлена работа с картинками

--- ssoboi/ssoboi/urls.py
+++ ssoboi/ssoboi/urls.py
@@ -18,6 +18,7 @@ from django.contrib import admin
 from django.conf.urls.static import static
 from django.conf import settings
 
+
 urlpatterns = [
     url(r'admin/doc/', include('django.contrib.admindocs.urls')),
     url(r'^admin/', admin.site.urls),
